0fbd73c3a35267cf24559c70033b2fa3007540cf
Ashlock-rng <reskyashlock@gmail.com>
2026-08-29 20:02:30 +0100

    Create gift_catalog_seed.dart

---
+++ lib/models/lib/models/lib/config/gift_catalog_seed.dart
@@ -0,0 +1,13 @@
+const List<Map<String, dynamic>> defaultGiftCatalog = [
+  {'name': 'Rose', 'priceUsd': 10.0, 'imageUrl': 'assets/gifts/rose.png'},
+  {'name': 'Heart', 'priceUsd': 25.0, 'imageUrl': 'assets/gifts/heart.png'},
+  {'name': 'Diamond', 'priceUsd': 50.0, 'imageUrl': 'assets/gifts/diamond.png'},
+  {'name': 'Sports Car', 'priceUsd': 100.0, 'imageUrl': 'assets/gifts/car.png'},
+  {'name': 'Yacht', 'priceUsd': 500.0, 'imageUrl': 'assets/gifts/yacht.png'},
+  {'name': 'Private Plane', 'priceUsd': 1000.0, 'imageUrl': 'assets/gifts/plane.png'},
+  {'name': 'Private Jet', 'priceUsd': 5000.0, 'imageUrl': 'assets/gifts/jet.png'},
+  {'name': 'Rocket', 'priceUsd': 10000.0, 'imageUrl': 'assets/gifts/rocket.png'},
+  {'name': 'Castle', 'priceUsd': 25000.0, 'imageUrl': 'assets/gifts/castle.png'},
+  {'name': 'Island', 'priceUsd': 50000.0, 'imageUrl': 'assets/gifts/island.png'},
+  {'name': 'Space Station', 'priceUsd': 100000.0, 'imageUrl': 'assets/gifts/space.png'},
+];
